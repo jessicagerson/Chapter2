07df6c468f003cd00c0886bfd8803734967256d0
Gerson <2006640@student.acalanes.k12.ca.us>
2017-10-03 12:56:04 -0700

    Day One

---
+++ Paint.java
@@ -0,0 +1,33 @@
+//***************************************************************
+//File: Paint.java
+//
+//Purpose: Determine how much paint is needed to paint the walls
+//of a room given its length, width, and height
+//***************************************************************
+ 
+public class Paint
+{
+	public static void main(String[] args)
+	{
+    	 //final int COVERAGE = 350;  //paint covers 350 sq ft/gal
+	 //declare integers length, width, and height;
+	 int length = 15;
+	 int width = 11;
+	 int height = 8;
+    	 //declare double totalSqFt;
+    	 double totalSqFt = (width * height * 2) + (length * height * 2) + (width * length);
+    	 //declare double paintNeeded;
+    	 
+    	 //double paintNeeded = 
+ 
+    	 
+ 
+    	 //Compute the total square feet to be painted--think
+    	 //about the dimensions of each wall
+ 
+    	 //Compute the amount of paint needed
+ 
+    	 //Print the length, width, and height of the room and the
+    	 //number of gallons of paint needed.
+	}
+}
